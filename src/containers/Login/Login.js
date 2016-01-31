013e64a6b70995fb1cbb9f38ddf872814019fae7
Anton Ovechkin <owanturist@gmail.com>
2016-01-31 23:09:56 +0600

    Made client request with cookie with fetch! eeeahh!

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -4,20 +4,60 @@ import DocumentMeta from 'react-document-meta';
 import * as authActions from 'reducers/auth';
 import config from '../../config';
 
+import fetch from 'isomorphic-fetch';
+
+const LOGIN = 'redux-example/auth/LOGIN';
+const LOGIN_SUCCESS = 'redux-example/auth/LOGIN_SUCCESS';
+const LOGIN_FAIL = 'redux-example/auth/LOGIN_FAIL';
+
+function formatUrl(apiPath = '') {
+    // remove all matched '/' from the start
+    const adjustedPath = apiPath.replace(/^\/*/, '');
+
+    return __SERVER__
+        // prepend host and port of the API server to the path.
+        ? `http://${config.apiHost}:${config.apiPort}/${adjustedPath}`
+        // prepend `/api` to relative URL, to proxy to API server.
+        : `/api/${adjustedPath}`;
+}
+
+export function loginFetch(name) {
+    return {
+        type: [LOGIN, LOGIN_SUCCESS, LOGIN_FAIL],
+        payload: () => fetch(formatUrl('/login'), {
+            method: 'post',
+            credentials: 'same-origin',
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify({name})
+        })
+            .then(response => response.json())
+    };
+}
+
 @connect(
   state => ({user: state.auth.user}),
-  authActions)
+  {...authActions, loginFetch})
 export default class Login extends Component {
   static propTypes = {
     user: PropTypes.object,
     login: PropTypes.func,
-    logout: PropTypes.func
+    logout: PropTypes.func,
+    loginFetch: PropTypes.func
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
     const input = this.refs.username;
     this.props.login(input.value);
+    input.value = '';
+  }
+
+  handleSubmitFetch = (event) => {
+    event.preventDefault();
+    const input = this.refs.username;
+    this.props.loginFetch(input.value);
     input.value = '';
   }
 
@@ -35,6 +75,8 @@ export default class Login extends Component {
               <input type="text" ref="username" placeholder="Enter a username" className="form-control"/>
             </div>
             <button className="btn btn-success" onClick={this.handleSubmit}><i className="fa fa-sign-in"/>{' '}Log In
+            </button>
+            <button className="btn btn-success" onClick={this.handleSubmitFetch}><i className="fa fa-sign-in"/>{' '}Log In Fetch
             </button>
           </form>
           <p>This will "log you in" as this user, storing the username in the session of the API server.</p>
